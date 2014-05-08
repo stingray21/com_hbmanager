78d7ccb9609aa474ea4be66e8726e60e35a307b8
Jochen Stehle <josh@stingray21.fake>
2014-05-08 15:12:09 +0200

    first commit

---
+++ site/views/hbofficials/tmpl/default.php
@@ -0,0 +1,30 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+
+
+echo '<h1>Funktionäre</h1>'."\n\n";
+
+foreach ($this->officials as $item)
+{
+	echo '<div class="funkblock">'."\n";
+	echo '<h3>'.$item->amt.'</h3>'."\n";
+	echo '<p class="name">'.$item->name;
+	echo '</p>'."\n";
+	if ($item->address.$item->postcode.$item->suburb != '') {
+		echo '<address>';
+		echo $item->address;
+		echo '<br />';
+		echo $item->postcode.' '.$item->suburb;
+		echo '</address>'."\n";
+	}
+	if (isset($item->contact))
+	{	
+		echo '<p>';
+		echo $item->contact;
+		echo '</p>'."\n";
+	}
+	echo '</div>';
+	echo "\n";
+}	
