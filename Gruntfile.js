73aa0590277e22c12c193961051f405f3ed6e33e
Josh <stingray@gmx.de>
2017-09-16 09:42:42 -0700

    Fixed grunt settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,54 +3,139 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        joomla_packager: {
-            com_hbteam: {
+        sync: {
+            source: {
+                files: [{
+                    cwd: '<%= ext.joomla %><%= ext.package %><%= ext.name %>/', // makes all src relative to cwd
+                    //cwd: '../../../xampp/htdocs/handball/hb_joomla3/' + function(){console.log('test'); return 'zzz_packages/';} + 'mod_tsvevents/',
+                    //cwd: '../../../xampp/htdocs/handball/hb_joomla3/' + 'zzz_packages/' + 'mod_tsvevents/',
+                    src: [
+                        '**'
+                    ],
+                    dest:  './<%= ext.name %>/',  
+                }],
+                pretend: false, // Don't do any IO. Before you run the task with `updateAndDelete` PLEASE MAKE SURE it doesn't remove too much. 
+                verbose: false // Display log messages when copying files 
+            }
+        },
+        'string-replace': {
+            version: {
+                files: {
+                    './<%= ext.name %>/': './<%= ext.name %>/<%= ext.name %>.xml',    // 'a': 'b' means b is source file and a is destination file 
+                },
                 options: {
-                  'name': 'hbteam',
-                  'type': 'component',
-                  //'joomla': '/home/josh/lamp/public_html/handball/hb_joomla3',
-                  'joomla': 'D:/xampp/htdocs/handball/hb_joomla3', // win Asus
-                  //'dest': '/home/josh/joomla-extensions/com_hbmanager/Source'      
-                  'dest': '/com_hbmanager/Source' // win Asus       
+                    replacements: [{
+                            pattern: /<version>.+?<\/version>/g,
+                            replacement: '<version><%= pkg.version %></version>'
+                        },
+                        {
+                            pattern: /<creationDate>.+?<\/creationDate>/g,
+                            replacement: '<creationDate><%= pkg.date %></creationDate>'
+                        }]
                 }
             }
+        },
+        bumpup: {
+            options: {
+                dateformat: 'YYYY-MM-DD HH:mm Z',
+                normalize: false,
+                updateProps: {
+                    pkg: 'package.json'
+                }
+            },
+            setters: {
+                // Overwrites setter for `date` property 
+                date: function (old, releaseType, options) {
+                    return grunt.template.today('yyyy-mm-dd HH:MM Z');   // local timezone 
+                },
+            },
+            files: ['package.json']
         },
         compress: {
             main: {
                 options: {
-                  //archive: '../Releases/com_hbteam.zip'
-                  archive: '../Releases/com_hbmanager_' + grunt.template.today('yyyymmdd_HHMMss') + '.zip'
+                  archive: '../Releases/<%= ext.name %>_' + grunt.template.today('yyyymmdd_HHMMss') + '.zip'
                 },
                 files: [
                     {
                     expand: true,
-                    cwd: 'com_hbmanager',
+                    cwd: '<%= ext.name %>',
                     src: '**/*',
                     },
                 ]
             }
-        }
+        },
+
     });
 
-    grunt.loadNpmTasks('grunt-joomla-packager');
     grunt.loadNpmTasks('grunt-contrib-compress');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
-    // grunt.loadNpmTasks('grunt-spritesmith');
-    // grunt.loadNpmTasks('grunt-contrib-copy');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-sass');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
-    // grunt.loadNpmTasks('grunt-postcss');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sync');
+    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-bumpup');
 
+    // load path from separate json file that is not synced with git
+    grunt.config.set('ext', grunt.file.readJSON('ext.json'));
+    // --> ext.json => e.g.
+    // {
+    //     "joomla": "D:\\xampp\\htdocs\\handball\\hb_joomla3\\",
+    //     "package": "zzz_packages/"
+    // }
+
+    /// ------------
 
     grunt.registerTask('default', ['compress']);
-    grunt.registerTask('build', ['joomla_packager:com_hbteam', 'compress']);
-    // grunt.registerTask('build', ['clean:build', 'concat:main', 'uglify:build', 'sass:build', 'postcss:dist', 'cssmin', 'copy:build', 'copy:buildmin']);
-    // grunt.registerTask('dev', ['clean:build', 'concat:main', 'sass:dev', 'postcss:dist', 'copy:dev']);
-    // grunt.registerTask('devplusmin', ['clean:build', 'concat:main', 'sass:dev', 'postcss:dist', 'copy:dev', 'uglify:build', 'sass:build', 'postcss:dist', 'cssmin', 'copy:buildmin']);
-    // grunt.registerTask('sprite-all', ['sprite:all']);
-    // grunt.registerTask('compsass', ['sass:dev', 'postcss:dist']);
+    grunt.registerTask('import', ['run-grunt', 'sync:source']);
+
+
+    grunt.registerTask('build_sub', 'Build Joomla extension', function(build) {
+        if (build === undefined) {
+            // grunt.warn('Building patch (build:patch)');
+            grunt.log.writeln('Building patch (build:patch)');
+            build = 'patch';
+        }
+        grunt.log.writeln('Build ' + build + ' version');
+            
+        grunt.task.run('bumpup:' + build);
+        grunt.task.run('string-replace:version');
+        grunt.task.run('compress');
+    });
+
+
+    grunt.registerTask('build', 'Import and build Joomla extension', function(build) {
+        if (build === undefined) {
+            // grunt.warn('Building patch (build:patch)');
+            grunt.log.writeln('Building patch (build:patch)');
+            build = 'patch';
+        }
+        grunt.log.writeln('Build ' + build + ' version');
+        
+        grunt.task.run('import');    
+        grunt.task.run('bumpup:' + build);
+        grunt.task.run('string-replace:version');
+        grunt.task.run('compress');
+    });
+
+
+
+    // run grunt file in joomla folder
+    grunt.registerTask('run-grunt', function() {
+        var ext = require('./ext.json');
+        // console.log(paths);
+        var cb = this.async();
+        var child = grunt.util.spawn({
+            grunt: true,
+            args: ['build_' + ext.name],
+            opts: {
+                cwd: ext.joomla
+                }
+        }, function(error, result, code) {
+          cb();
+        });
+
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+    });
+
+
 
 };
