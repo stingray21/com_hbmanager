c392ce3f4108de7f3fad3bf5e2f8de3c712eec1f
Jochen Stehle <joshsteele21@gmail.com>
2017-09-23 15:17:27 -0700

    moved hbofficials to main files

--- com_hbmanager/admin/sql/install.mysql.utf8.sql
+++ com_hbmanager/admin/sql/install.mysql.utf8.sql
@@ -216,3 +216,13 @@ CREATE TABLE IF NOT EXISTS `hb_updatelog` (
   `datum` datetime NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=262 ;
+
+
+CREATE TABLE IF NOT EXISTS `hb_funktionaer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alias` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `amt` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `emailalias` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
