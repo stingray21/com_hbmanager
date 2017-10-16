0dd6c1d7d1ce07d0fa35eaf237e22b6a896fc9e5
Jochen Stehle <joshsteele21@gmail.com>
2017-10-16 09:12:50 -0700

    hb_manager_old: remove parts that are already implemented in v2

--- com_hbmanager_old/site/views/hbcronjob/tmpl/default_js.php
+++
@@ -1,8 +0,0 @@
-<?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
-
-
-echo '<div id="eggtimer"><img src="./media/com_hbmanager/images/eggtimer.gif" /></div>';
-echo '<div id="hvwupdate">';
-echo '</div>';
