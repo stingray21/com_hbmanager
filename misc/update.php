0abae89c13f16b29f40fd98290cee01043b37526
Jochen Stehle <joshsteele21@gmail.com>
2018-05-06 11:20:19 +0200

    added remote update page

---
+++ misc/update.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>HKOG update HVW data</title>
+<meta http-equiv="refresh" content="2; URL=https://www.hkog.de/index.php/component/hbmanager/?view=update">
+<meta name="keywords" content="automatic redirection">
+</head>
+<body>
+
+<p>If your browser doesn't automatically go there within a few seconds, 
+you may want to go to 
+<a href="https://www.hkog.de/index.php/component/hbmanager/?view=update">update HVW data</a> 
+manually.</p>
+
+</body>
+</html>
